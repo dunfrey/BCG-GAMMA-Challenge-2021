e5fffe2e9e4639c5ae80be1daa94c60b26861a0f
Fernando Felix <fernandofelix@copin.ufcg.edu.br>
2021-05-22 19:09:09 -0300

    create notebook to define target population dataset

--- src/config.py
+++ src/config.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 data_dir = Path('BCG-GAMMA-Challenge-2021')
 data_path = data_dir / 'doc/raw_data_catalog.csv'
+
+target_ufs = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
