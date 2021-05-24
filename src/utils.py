18697c352a26d63a2c87e7088087964ff2367d74
Fernando Felix <fernandofelix@copin.ufcg.edu.br>
2021-05-24 07:48:48 -0300

    answering some questions and extracting new insights for hypothesis 1.2 and 1.3

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+from collections import Counter
+
+import pandas as pd
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -35,3 +39,36 @@ def plot_bar(df, x, y, rotation=90, palette="Blues_d"):
 
     plt.xticks(rotation=rotation)
     plt.tight_layout()
+
+
+def underscore_to_camelcase(word):
+    '''
+    https://stackoverflow.com/questions/40454141/capitalize-first-letter-only-of-a-string-in-python
+    '''
+    if '_' in word:
+        word = ''.join(x.capitalize() or '_' for x in word.split('_'))
+        return word[0].lower() + word[1:]
+
+    return word
+
+
+def strat_sample_from_base_dist(df, base_df, target_col):
+    '''
+    Generate stratified sample from a dataframe based on the distribuition of `base_df[target_col]`
+    '''
+    target_col_dist = Counter(base_df[target_col])
+
+    partial_sample_dfs = []
+
+    # pra cada faixaPopulacaoEstimada de municipios mais violentos, gerar mesmo numero de sample...
+    for label, label_size in target_col_dist.items():
+        _sample_df = df[(df[target_col] == label)]
+
+        if len(_sample_df) > label_size:
+            _sample_df = _sample_df.iloc[0:label_size]
+
+        partial_sample_dfs.append(_sample_df)
+
+    sample_df = pd.concat(partial_sample_dfs)
+
+    return sample_df
